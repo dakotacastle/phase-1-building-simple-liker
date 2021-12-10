d445b5deffb9f627a6470e595aae3e4d7fa9d67f
Dakota Castle <barrettcastle@gmail.com>
2021-12-10 15:17:23 -0500

    commit tree 09871f5f32f255c28cef83a208e8e21292470bf3

--- main.js
+++ main.js
@@ -2,10 +2,34 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  const clickEmptyHeart = document.querySelectorAll('.like-glyph')
+  clickEmptyHeart.forEach(heart => heart.addEventListener('click', func1))
+})
 
-
-
+function func1(e){
+  mimicServerCall()
+  .then(() => {
+    const heart = e.target
+    if (heart.innerText === EMPTY_HEART){
+      heart.innerText = FULL_HEART
+      heart.classList.add('activated-heart')
+    }
+    else if (heart.innerText === FULL_HEART){
+      heart.innerText = EMPTY_HEART
+      heart.classList.remove('activated-heart')
+    }
+  })
+  .catch(error => {
+    const modal = document.getElementById('modal')
+    const modalMsg = document.getElementById('modal-message')
+    modal.classList.remove('hidden')
+    modalMsg.innerText = error
+    setTimeout(() => {
+      modal.classList.add('hidden')
+    }, 3000)
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
